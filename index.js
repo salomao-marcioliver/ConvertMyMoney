fd498339d5816c2b4d78d08b4295063d05706695
Salomão Marcioliver <salomaomarcioliver@gmail.com>
2021-09-21 14:59:26 -0300

    Initial version

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+const { convert, toMoney } = require('./lib/convert')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get("/", (req, res) => {
+    res.render("home")
+});
+
+app.get("/cotacao", (req, res) => {
+    const { cotacao, quantidade } = req.query
+    if (cotacao && quantidade) {
+        const conversao = convert(cotacao, quantidade)
+        res.render('cotacao', { error: false, cotacao: toMoney(cotacao), quantidade: toMoney(quantidade), conversao: toMoney(conversao) })
+    }else{
+        res.render('cotacao', {error: 'Valores inválidos'})
+    }
+})
+
+app.listen(3000, error => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log("Servidor rodando!")
+    }
+})
